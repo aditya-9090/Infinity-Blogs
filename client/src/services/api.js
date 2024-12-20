5d001f54205f1a6f7b51e528b3d7636ada59f222
Aditya <adityapatil9537@gmail.com>
2024-12-20 17:16:53 +0530

    inserted api in url in frontend

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://infinity-blogs-backend.onrender.com', // Replace with your backend URL
+  baseURL: 'https://infinity-blogs-backend.onrender.com/api', // Replace with your backend URL
 });
 
 api.interceptors.request.use((config) => {
